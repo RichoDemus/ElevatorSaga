4968eb892f437d11ee0e95de5c2c1477ae34aa14
RichoDemus <richodemus@gmail.com>
2015-02-01 16:21:55 +0100

    started working on new logic

--- main.js
+++ main.js
@@ -17,21 +17,42 @@
             
             elevator.on("floor_button_pressed", function(floorNum)
             {
-                elevator.goToFloor(floorNum);
+                //elevator.goToFloor(floorNum);
+                //should be added to some sorted list or something, 
+                //so that the elevator can check this list just before it passes a floor
+            });
+            
+            /**
+            * Triggered slightly before the elevator will pass a floor. 
+            * A good time to decide whether to stop at that floor. 
+            * Note that this event is not triggered for the destination floor. 
+            * Direction is either "up" or "down".
+            */
+            elevator.on("passing_floor", function(floorNum, direction)
+            {
+                //todo impl    
+            });
+            
+            elevator.on("stopped_at_floor", function(floorNum)
+            {
+                // Maybe decide where to go next?
             });
         });
         
         floors.forEach(function(floor)
         {
+            floor.peopleWaitingToGoUp = 0;
+            floor.peopleWaitingToGoDown = 0;
+            
             _this.allFloors[floor.floorNum] = floor;
             floor.on("up_button_pressed", function()
             {
-                _this.elevatorButtonPressed(floor);
+                floor.peopleWaitingToGoUp++;
             });
             
             floor.on("down_button_pressed", function()
             {
-                _this.elevatorButtonPressed(floor);
+                floor.peopleWaitingToGoDown++;
             });
         });
                 
